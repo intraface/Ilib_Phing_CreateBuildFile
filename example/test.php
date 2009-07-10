97bd2f66f3f05ad078e299adc0e745a38106da45
Sune T. Jensen <sj@sunet.dk>
2009-07-10 13:51:32 +0000

    a test on public.intraface.dk

---
+++ example/test.php
@@ -0,0 +1,15 @@
+<?php
+
+
+require_once 'PEAR/PackageUpdate.php';
+$ppu = PEAR_PackageUpdate::factory('Cli', 'Ilib_Countries', 'public.intraface.dk');
+if($ppu != false) {
+    fwrite(STDOUT,"FETCHING LATEST RELEASE. Please wait...\n");
+    
+    $release = $ppu->getLatestRelease();
+    if($release != false) {
+        fwrite(STDOUT,"LATEST RELEASE: ".$release['state']." ".$release['version']."\n");
+    }
+}
+die("finish\n");
+?>
